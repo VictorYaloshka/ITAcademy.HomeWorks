07a7a293de7ea2142dbe033bababf13072bd28d2
Jayeti1331 <jayeti@mail.ru>
2020-03-01 12:50:12 +0300

    completed 'Program.cs'

--- Solution/HW.02/Program.cs
+++ Solution/HW.02/Program.cs
@@ -14,6 +14,18 @@ namespace HW._02
             //5. Конвертация каждого элемента массива 'arrayOfTextResult.Length - 1' с помощью цикла в байты и сборка их в массив 'imageBytes'.
             //6. Вызов метода 'Dispose' для освобождения от неуправляемых ресурсов 'textReader'.
             //7. Создание нового файла по пути "C:\Temp\image.png" с помощью метода 'WriteAllBytes'.
+
+            StreamReader textReader = new StreamReader(@"C:\Temp\image.txt", true);
+            string textReaderResult = textReader.ReadToEnd();
+            string[] arrayOfTextResult = textReaderResult.Split(' ');
+            byte[] imageBytes = new byte[arrayOfTextResult.Length - 1];
+            for (int i = 0; i < arrayOfTextResult.Length - 1; i++)
+            {
+                byte binary = Convert.ToByte(arrayOfTextResult[i], 2);
+                imageBytes[i] = binary;
+            }
+            textReader.Dispose();
+            File.WriteAllBytes(@"C:\Temp\image.png", imageBytes);
         }
     }
 }
