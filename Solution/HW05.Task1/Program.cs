c78c94c7cd09f3d113a178d5e26da27ed5d25db0
Jayeti1331 <jayeti@mail.ru>
2020-03-19 23:24:15 +0300

    add task 1

---
+++ Solution/HW05.Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HW05.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 90, a = 122; i > 64 & a>96; i--,a--)
+            {
+                char letter = (char)(int)i;
+                char letter2 = (char)(int)a;
+                Console.Write(letter);
+                Console.WriteLine(letter2);
+            }
+           
+        }
+    }
+}
