92f446e0d2cfe4955a730893388cf147c21ae0f3
Jayeti1331 <jayeti@mail.ru>
2020-03-28 16:14:34 +0300

    add HW08.Task01

---
+++ Solution/HW08.Task01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HW08.Task01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Enter a poem:");
+            string str = Convert.ToString(Console.ReadLine());
+            str = str.Replace("О", "А");
+            string[] array1 = str.Split(';');
+
+            Console.WriteLine();
+            Console.WriteLine("The poem after formatting:");
+            foreach (string item in array1)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
