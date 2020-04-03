e3a498bc65efc1f69850e422161244448baa8eb5
Jayeti1331 <jayeti@mail.ru>
2020-04-03 19:28:45 +0300

    fix hm_09

---
+++ Solution/HW09.Task01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HW09.Task01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
