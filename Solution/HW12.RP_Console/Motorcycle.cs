87c08e0e0d3166b7812ffb8ddc6e131a6eeb1933
Jayeti1331 <jayeti@mail.ru>
2020-04-25 15:07:24 +0300

    add home work

---
+++ Solution/HW12.RP_Console/Motorcycle.cs
@@ -0,0 +1,28 @@
+﻿
+namespace HW12.RP_Console
+{
+    public class Motorcycle
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int Odometer { get; set; }
+        public int Price { get; set; }
+
+
+        public Motorcycle(int Id, string Name, string Model, int Year, int Odometer, int Price)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Model = Model;
+            this.Year = Year;
+            this.Odometer = Odometer;
+            this.Price = Price;
+        }
+
+        public Motorcycle()
+        {
+        }
+    }
+}
---
+++ Solution/HW12.RP_Console/Program.cs
@@ -0,0 +1,50 @@
+﻿using HW12.RP_DAL;
+using log4net;
+using log4net.Config;
+
+[assembly: XmlConfigurator(ConfigFile = "app.config", Watch = true)]
+
+namespace HW12.RP_Console
+{
+    class Program
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(Program));
+
+        static void Main(string[] args)
+        {
+            log.Info("Start of the programm");
+
+            MotoStaticCollection motoStaticCollection = new MotoStaticCollection();
+
+            motoStaticCollection.CreateMotorcycle(new Motorcycle()
+            {
+                Id = 05,
+                Name = "Harley Davidson",
+                Model = "Cosmic Starship",
+                Year = 2000,
+                Odometer = 2_000,
+                Price = 2_000_000
+            });
+
+            motoStaticCollection.GetMotorcycleByID(05);
+
+            motoStaticCollection.GetMotorcycles();
+
+            motoStaticCollection.UpdateMotorcycle(new Motorcycle()
+            {
+                Id = 02,
+                Name = "Dodge",
+                Model = "Tomahawk",
+                Year = 1995,
+                Odometer = 300,
+                Price = 550_000
+            });
+
+            motoStaticCollection.DeleteMotorcycle(04);
+
+            motoStaticCollection.GetMotorcycles();
+
+            log.Info("End of the programm");
+        }
+    }
+}
